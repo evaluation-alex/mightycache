3e28b03eb13cde3cf3f8483c47d7039d72543731
David Pate <davidtpate@gmail.com>
2015-06-29 16:38:21 -0400

    Remove the required keys for AWS Access Key and AWS Secret Key as these can be obtained in multiple ways when using AWS.
    Add specific sets of tests around dynamic credentials and hardcoded credentials.

--- test/s3.js
+++ test/s3.js
@@ -23,7 +23,7 @@
  */
 (function (expect, lib) {
     'use strict';
-    describe('S3 Specific Implementation', function () {
+    describe.only('S3 Specific Implementation', function () {
         it('Shouldn\'t be able to instantiate the S3 cache implementation without a bucket', function () {
             expect(function () {
                 lib.cache('s3', {});
@@ -37,46 +37,6 @@
                     }
                 );
             }).to.throw(Error, 'Invalid Argument Type Expected [string] for [bucket] but got [object]');
-        });
-        it('Shouldn\'t be able to instantiate the S3 cache implementation without an access key id', function () {
-            expect(function () {
-                lib.cache('s3',
-                    {
-                        bucket: 'test'
-                    }
-                );
-            }).to.throw(Error, 'Missing Required Argument [accessKeyId]');
-        });
-        it('Shouldn\'t be able to instantiate the S3 cache implementation with an invalid access key id', function () {
-            expect(function () {
-                lib.cache('s3',
-                    {
-                        bucket: 'test',
-                        accessKeyId: {}
-                    }
-                );
-            }).to.throw(Error, 'Invalid Argument Type Expected [string] for [accessKeyId] but got [object]');
-        });
-        it('Shouldn\'t be able to instantiate the S3 cache implementation without a secret access key', function () {
-            expect(function () {
-                lib.cache('s3',
-                    {
-                        bucket: 'test',
-                        accessKeyId: 'test'
-                    }
-                );
-            }).to.throw(Error, 'Missing Required Argument [secretAccessKey]');
-        });
-        it('Shouldn\'t be able to instantiate the S3 cache implementation with an invalid secret access key', function () {
-            expect(function () {
-                lib.cache('s3',
-                    {
-                        bucket: 'test',
-                        accessKeyId: 'test',
-                        secretAccessKey: {}
-                    }
-                );
-            }).to.throw(Error, 'Invalid Argument Type Expected [string] for [secretAccessKey] but got [object]');
         });
         it('Should instantiate the S3 cache implementation', function () {
             var cache = lib.cache('s3',
